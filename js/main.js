92aeac9a6668056f9558194e3db69cff7c35223f
Deng Haolin <1020958553@qq.com>
2017-04-13 14:31:35 +0800

    add icons

--- js/main.js
+++ js/main.js
@@ -13,221 +13,275 @@ var teams = [
     home: 'Philips Arena',
     position: {lat: 33.75728, lng: -84.39632},
     conf: 'East',
-    div: 'Southeast'
+    div: 'Southeast',
+    marker: {},
+    icon: 'css/img/1.png'
   },
   {
     team: 'Boston Celtics',
     home: 'TD Banknorth Garden',
     position: {lat: 42.366198, lng: -71.06214599999998},
     conf: 'East',
-    div: 'Atlantic'
+    div: 'Atlantic',
+    marker: {},
+    icon: 'css/img/2.png'
   },
   {
     team: 'Brooklyn Nets',
     home: 'Barclays Center',
     position: {lat: 40.6824952, lng: -73.97503490000003},
     conf: 'East',
-    div: 'Atlantic'
+    div: 'Atlantic',
+    marker: {},
+    icon: 'css/img/17.png'
   },
   {
     team: 'Charlotte Hornets',
     home: 'Time Warner Cable Arena',
     position: {lat: 35.2251426, lng: -80.8392351},
     conf: 'East',
-    div: 'Southeast'
+    div: 'Southeast',
+    marker: {},
+    icon: 'css/img/30.png'
   },
   {
     team: 'Chicago Bulls',
     home: 'United Center',
     position: {lat: 41.8806908, lng: -87.67417590000002},
     conf: 'East',
-    div: 'Central'
+    div: 'Central',
+    marker: {},
+    icon: 'css/img/4.png'
   },
   {
     team: 'Cleveland Cavaliers',
     home: 'Quicken Loans Arena',
     position: {lat: 41.4965597, lng: -81.6882172},
     conf: 'East',
-    div: 'Central'
+    div: 'Central',
+    marker: {},
+    icon: 'css/img/5.png'
   },
   {
     team: 'Dallas Mavericks',
     home: 'American Airlines Center',
     position: {lat: 32.7905064, lng: -96.8103549},
     conf: 'West',
-    div: 'Southwest'
+    div: 'Southwest',
+    marker: {},
+    icon: 'css/img/6.png'
   },
   {
     team: 'Denver Nuggets',
     home: 'Pepsi Center',
     position: {lat: 39.74866, lng: -105.00770999999997},
     conf: 'West',
-    div: 'Northwest'
+    div: 'Northwest',
+    marker: {},
+    icon: 'css/img/7.png'
   },
   {
     team: 'Detroit Pistons',
     home: 'The Palace of Auburn Hills',
     position: {lat: 42.69688499999999, lng: -83.24579289999997},
     conf: 'East',
-    div: 'Central'
+    div: 'Central',
+    marker: {},
+    icon: 'css/img/8.png'
   },
   {
     team: 'Golden State Warriors',
     home: 'Oracle Arena',
     position: {lat: 37.7502917, lng: -122.2030494},
     conf: 'West',
-    div: 'Pacific'
+    div: 'Pacific',
+    marker: {},
+    icon: 'css/img/901.png'
   },
   {
     team: 'Houston Rockets',
     home: 'Toyota Center',
     position: {lat: 29.75076039999999, lng: -95.3621005},
     conf: 'West',
-    div: 'Southwest'
+    div: 'Southwest',
+    marker: {},
+    icon: 'css/img/10.png'
   },
   {
     team: 'Indiana Pacers',
     home: 'Bankers Life Fieldhouse',
     position: {lat: 39.7639301, lng: -86.15546410000002},
     conf: 'East',
-    div: 'Central'
+    div: 'Central',
+    marker: {},
+    icon: 'css/img/11.png'
   },
   {
     team: 'LA Clippers',
     home: 'Staples Center',
     position: {lat: 34.0430175, lng: -118.2672541},
     conf: 'West',
-    div: 'Pacific'
+    div: 'Pacific',
+    marker: {},
+    icon: 'css/img/1201.png'
   },
   {
     team: 'Los Angeles Lakers',
     home: 'Staples Center',
     position: {lat: 34.0430175, lng: -118.2672541},
     conf: 'West',
-    div: 'Pacific'
+    div: 'Pacific',
+    marker: {},
+    icon: 'css/img/13.png'
   },
   {
     team: 'Memphis Grizzlies',
     home: 'FedExForum',
     position: {lat: 35.1382037, lng: -90.05047379999996},
     conf: 'West',
-    div: 'Southwest'
+    div: 'Southwest',
+    marker: {},
+    icon: 'css/img/29.png'
   },
   {
     team: 'Miami Heat',
     home: 'American Airlines Arena',
     position: {lat: 25.7814014, lng: -80.18696899999998},
     conf: 'East',
-    div: 'Southeast'
+    div: 'Southeast',
+    marker: {},
+    icon: 'css/img/14.png'
   },
   {
     team: 'Milwaukee Bucks',
     home: 'Bradley Center',
     position: {lat: 43.0436509, lng: -87.9171991},
     conf: 'East',
-    div: 'Central'
+    div: 'Central',
+    marker: {},
+    icon: 'css/img/15.png'
   },
   {
     team: 'Minnesota Timberwolves',
     home: 'Target Center',
     position: {lat: 44.9794545, lng: -93.27618989999996},
     conf: 'West',
-    div: 'Northwest'
+    div: 'Northwest',
+    marker: {},
+    icon: 'css/img/16.png'
   },
   {
     team: 'New Orleans Pelicans',
     home: 'NewOrleans Arena',
     position: {lat: 29.9490351, lng: -90.08205679999998},
     conf: 'West',
-    div: 'Southwest'
+    div: 'Southwest',
+    marker: {},
+    icon: 'css/img/3.png'
   },
   {
     team: 'New York Knicks',
     home: 'Madison Square Garden',
     position: {lat: 40.7505045, lng: -73.99343870000001},
     conf: 'East',
-    div: 'Atlantic'
+    div: 'Atlantic',
+    marker: {},
+    icon: 'css/img/18.png'
   },
   {
     team: 'Oklahoma City Thunder',
     home: 'Chesapeake Energy Arena',
     position: {lat: 35.4634175, lng: -97.5151176},
     conf: 'West',
-    div: 'Northwest'
+    div: 'Northwest',
+    marker: {},
+    icon: 'css/img/25.png'
   },
   {
     team: 'Orlando Magic',
     home: 'Amway Arena',
     position: {lat: 28.5392562, lng: -81.3838599},
     conf: 'East',
-    div: 'Southeast'
+    div: 'Southeast',
+    marker: {},
+    icon: 'css/img/19.png'
   },
   {
     team: 'Philadelphia 76ers',
     home: 'Wells Fargo Center',
     position: {lat: 39.9012015, lng: -75.17197950000002},
     conf: 'East',
-    div: 'Atlantic'
+    div: 'Atlantic',
+    marker: {},
+    icon: 'css/img/20.png'
   },
   {
     team: 'Phoenix Suns',
     home: 'U.S Airways Center',
     position: {lat: 33.4457369, lng: -112.0712006},
     conf: 'West',
-    div: 'Pacific'
+    div: 'Pacific',
+    marker: {},
+    icon: 'css/img/21.png'
   },
   {
     team: 'Portland Trail Blazers',
     home: 'Moda Center',
     position: {lat: 45.53156509999999, lng: -122.66684229999998},
     conf: 'West',
-    div: 'Northwest'
+    div: 'Northwest',
+    marker: {},
+    icon: 'css/img/22.png'
   },
   {
     team: 'Sacramento Kings',
     home: 'Power Balance Pavilion',
     position: {lat: 38.6490388, lng: -121.51812840000002},
     conf: 'West',
-    div: 'Pacific'
+    div: 'Pacific',
+    marker: {},
+    icon: 'css/img/23.png'
   },
   {
     team: 'San Antonio Spurs',
     home: 'AT&T Center',
     position: {lat: 29.4270706, lng: -98.43744549999997},
     conf: 'West',
-    div: 'Southwest'
+    div: 'Southwest',
+    marker: {},
+    icon: 'css/img/24.png'
   },
   {
     team: 'Toronto Raptors',
     home: 'Air Canada Centre',
     position: {lat: 43.6434661, lng: -79.37909890000003},
     conf: 'East',
-    div: 'Atlantic'
+    div: 'Atlantic',
+    marker: {},
+    icon: 'css/img/28.png'
   },
   {
     team: 'Utah Jazz',
     home: 'Energy Solutions',
     position: {lat: 40.7682548, lng: -111.9011016},
     conf: 'West',
-    div: 'Northwest'
+    div: 'Northwest',
+    marker: {},
+    icon: 'css/img/26.png'
   },
   {
     team: 'Washington Wizards',
     home: 'Verizon Center',
     position: {lat: 38.89812510000001, lng: -77.02088040000001},
     conf: 'East',
-    div: 'Southeast'
+    div: 'Southeast',
+    marker: {},
+    icon: 'css/img/27.png'
   }
 ];
 
 //newList
 var newsList = [];
-
-//array store the teams that will be display
-var displayteams = [].concat(teams);
-
-//array store the markers on the map
-var markers = [];
 
 //view model
 var viewModel = function() {
@@ -236,7 +290,7 @@ var viewModel = function() {
   self.fliterOn = ko.observable(true);
   self.windowTitle = ko.observable(bigTitle);
   self.fliter = ko.observableArray(conference);
-  self.teamsList = ko.observableArray(displayteams);
+  self.teamsList = ko.observableArray(teams);
   self.newsList = ko.observableArray(newsList);
   self.showNewsList = ko.observable(false);
 
@@ -247,53 +301,33 @@ var viewModel = function() {
       zoom: 15
     });
 
-    //style of defaultIcon
-    defaultIcon = makeMarkerIcon('0091ff');
-
     bounds = new google.maps.LatLngBounds();
-
     renderMarkers();
-
-    function makeMarkerIcon(markerColor) {
-      var markerImage = new google.maps.MarkerImage(
-        'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
-        '|40|_|%E2%80%A2',
-        new google.maps.Size(21, 34),
-        new google.maps.Point(0, 0),
-        new google.maps.Point(10, 34),
-        new google.maps.Size(21,34));
-      return markerImage;
-    }
   };
+
 
   self.renderMarkers = function() {
     console.log('startRendering');
-    for (var i = 0; i < teamsList().length; i++) {
-      var position = teamsList()[i].position;
-      var title = teamsList()[i].home;
-      var team = teamsList()[i].team;
-      //create a marker pre location
-      var marker = new google.maps.Marker({
-        position: position,
-        title: title,
-        team: team,
-        icon: defaultIcon,
+    teams.forEach(function(team, i){
+      team.marker = new google.maps.Marker({
+        position: team.position,
+        title: team.home,
+        team: team.team,
+        icon: team.icon,
         id: i
       });
       //show marker
-      marker.setMap(map);
-      //create an onclick event to open the infowindow at each marker
-      marker.addListener('click', function() {
+      team.marker.setMap(map);
+      //onclick event
+      team.marker.addListener('click', function() {
         showNewsList(true);
-        windowTitle(this.team);
+        windowTitle(this.title);
         popInfoWindow(this);
         requestNewsList(this.team);
       });
-      //push the marker into markers array
-      markers.push(marker);
       //set bounds
-      bounds.extend(marker.position);
-    }
+      bounds.extend(team.marker.position);
+    });
     map.fitBounds(bounds);
   };
 
